33dcbe9b9368cc5a1c037052fd84d75268ecc28a
TheInfoGuru <scates09@gmail.com>
2024-08-16 12:47:11 -0500

    Added functions to get book contents, word count, character counts, and a sorted sheet list of those counts.

---
+++ main.py
@@ -0,0 +1,45 @@
+import re
+
+def create_sorted_sheet(sorted_list: list) -> str:
+    text_block = ""
+    for letter_count in sorted_list:
+        text_block = text_block + f"The character '{letter_count[0]}' was found {letter_count[1]} times.\n"
+    return text_block
+
+def sort_character_counts(character_count: dict) -> list:
+    return [ (k, character_count[k]) for k in \
+                sorted(character_count, key=character_count.get, reverse=True) ]
+
+#Get the counts for each character in a supplied string
+def get_character_counts(text: str):
+    character_counts = {} #initialize dic
+    for k in text.lower(): #loop through each letter in the text
+        try:
+            character_counts[k] += 1 #add one to the char count
+        except KeyError as e:
+            character_counts[k] = 1 #if key doesn't exist yet, set count to 1
+    return character_counts
+
+#get the count of the words in a supplied string
+def get_word_count(text: str):
+    return len(re.findall(r'\S+', text)) #S+ is all words
+
+#get the text of a file at the supplied path
+def get_book_text(path: str):
+    if not path:
+        print("Error: no book path detected.")
+        exit(1)
+    with open(path) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_text(path=book_path)
+    word_count = get_word_count(text=book_contents)
+    character_counts = get_character_counts(text=book_contents)
+    sorted_character_count = sort_character_counts(character_count=character_counts)
+    text_sheet_p1 = create_sorted_sheet(sorted_character_count)
+    print(text_sheet_p1)
+    exit(0)
+
+main()
